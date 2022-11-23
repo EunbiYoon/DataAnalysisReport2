3dd15f4dc682638d3e4b384b390610447e550371
Eunbi Yoon <yinyinbei0717@gmail.com>
2022-11-23 16:08:57 -0600

    dfadf

--- app.py
+++ app.py
@@ -5,11 +5,23 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    return render_template('index.html', ABlabels=ABlabels,
+    Avalues2M=Avalues2M, Avalues1M=Avalues1M, Avalues0M=Avalues0M,
+    Bvalues2M=Bvalues2M,Bvalues1M=Bvalues1M,Bvalues0M=Bvalues0M,
+    legend2M=legend2M, legend1M=legend1M, legend0M=legend0M)
+
+@app.route("/charts")
+def charts():
     return render_template('charts.html', ABlabels=ABlabels,
     Avalues2M=Avalues2M, Avalues1M=Avalues1M, Avalues0M=Avalues0M,
     Bvalues2M=Bvalues2M,Bvalues1M=Bvalues1M,Bvalues0M=Bvalues0M,
     Plabels=Plabels,Pvalues=Pvalues,
     legend2M=legend2M, legend1M=legend1M, legend0M=legend0M)
+
+@app.route("/tables")
+def tables():
+    return render_template('tables.html')
+
 
 if __name__ == "__main__":
     app.run(debug=True)
